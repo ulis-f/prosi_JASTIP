461f3e19f7b0041b2c36d1e28dafa6a49b3d1726
Yulius Famas <yuliusfamas@gmail.com>
2020-04-16 15:48:11 +0700

    UPDATE FILE

--- view/layout/layout.php
+++ view/layout/layout.php
@@ -1,15 +1,4 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>PHP - <?php echo $title;?></title>
-		
-	</head>
-	<body>
-		<h1> Perpustakaan Hore </h1>
-		<?php echo $content;?>
-		<hr>
-			<a href="index.php">Rent</a>
-			<span> | </span>
-			<a href="info.php">Info</a>
-	</body>
+	
 </html>
