f6f5f419daa1392cccb25b153f16b080572a8ad4
Yulius Famas <yuliusfamas@gmail.com>
2020-04-20 09:53:24 +0700

    UPDATE

---
+++ view/gantiPassword.php
@@ -0,0 +1,21 @@
+
+<h1>Ganti Password</h1>
+<body>
+    <form action="gantiPassword" method="POST">
+            <table>
+                <tr>
+                    <td>Password Lama</td>
+                    <td>:</td>
+                    <td><input type="text" name="Lama"></td>
+                </tr>
+                <tr>
+                    <td>Password Baru</td>
+                    <td>:</td>
+                    <td><input type="text" name="Baru"></td>
+                </tr>
+			</table>
+                
+            <button type="submit">Submit</button>
+                 
+        </form>
+    </body>
