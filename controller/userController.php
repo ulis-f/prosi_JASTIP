3c91b8fc0ba59ca6f370b49186fec2fbebff6255
Quadrat <56858127+quadratnp@users.noreply.github.com>
2020-04-20 09:49:54 +0700

    UPDATE 20/04
    update password, login, hapus akun. sudah , tapi harus di sesuaikan sama UI

--- controller/userController.php
+++ controller/userController.php
@@ -9,6 +9,32 @@ class UserController{
 	public function __construct(){
 		$this->db = new MySQLDB("localhost","root","","titipaja");
 	}
+
+	public function login(){
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		$kondisi = false;
+
+		if (isset($username) && isset($password) && $username != "" && $password != "") {
+			$query = "SELECT * FROM user where namaUser = $username AND password = $password";
+			$query_result = $this->db->executeSelectQuery($query);
+			$cek = mysql_num_rows($query_result);
+
+			if($cek>0){
+				$_SESSION['username'] = $username;
+				$_SESSION['password'] = $password;
+				$kondisi = true;
+			}
+			
+			return $kondisi;
+		}
+
+	}
+
+	public function logout(){
+		session_unset();
+		session_destroy();
+	}
 	
 
 	public function register(){
@@ -23,18 +49,24 @@ class UserController{
 		}
 	}
 
-	public function login(){
-		$username = $_GET['username'];
-		$password = $_GET['password'];
-
-		if (isset($username) && isset($password) && $username != "" && $password != "") {
-			$id = $this->db->escapeString($id);
-			$username = $this->db->escapeString($username);
-			$query = "SELECT password WHERE namaUser LIKE $username";
-			$this->db->executeNonSelectQuery($query);
-		}
+	public function hapusAkun(){
+		$username = $_SESSION['username'];
+		$query = "DELETE FROM user WHERE namaUser = $username";
+		$query_result =$this->db->executeNonSelectQuery($query);
 	}
 
+	public function updatePass(){
+		$username = $_SESSION['username'];
+		$password = $_SESSION['password'];
+		$usernameBaru = $_POST['usernameBaru'];
+		$passwordBaru = $_POST['passwordBaru'];
+		$query = "UPDATE user 
+				SET namaUser = $usernameBaru, password = $passwordBaru 
+				WHERE namaUser = $username AND password = $password";
+		$query_result = $this->db->executeNonSelectQuery($query);
+		$_SESSION['username'] = $usernameBaru;
+		$_SESSION['password'] = $passwordBaru;
+	}
 	
 }
 
--- router.php
+++ router.php
@@ -1,11 +1,15 @@
 <?php
 	$url = $_SERVER['REDIRECT_URL'];
 	$baseURL = '/prosi_JASTIP/view';
+	session_start();
 
 	if ($_SERVER["REQUEST_METHOD"] == "GET"){
 		switch($url){
 			case $baseURL . '/register':
 				header('location: register.php');
+				break;
+			case $baseURL . '/login':
+				header('location: login.php');
 				break;
 			default:
 				echo '404 Not Found';
@@ -22,6 +26,29 @@
 				$roleCtrl = new userController();
 				$roleCtrl->register();
 				header('Location: register');
+				break;
+			case $baseURL.'/cekLogin':
+				require_once "controller/userController.php";
+				$roleCtrl = new userController();
+				$result=$roleCtrl->login();
+				if($result = true){
+					header('Location: index');
+				}
+				else{
+					header('Location: login')
+				}
+				break;
+			case $baseURL.'/hapusAkun':
+				require_once "controller/userController.php";
+				$roleCtrl = new userController();
+				$roleCtrl->hapusAkun();
+				header('Location: login');
+				break;
+			case $baseURL.'/updatePass':
+				require_once "controller/userController.php";
+				$roleCtrl = new userController();
+				$roleCtrl->updatePass();
+				header('Location: login');
 				break;
 			default:
 				echo '404 Not Found';
