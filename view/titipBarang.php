1e5dd6d266bb30aeca244111c35962257c9211ec
Yulius Famas <yuliusfamas@gmail.com>
2020-04-25 15:22:20 +0700

    add file

---
+++ view/titipBarang.php
@@ -0,0 +1,49 @@
+<div class="container">
+        <div class="w3-card-4 w3-white" style="width:60%; margin: auto; padding: 50px; height: 750px; ">
+            <h2>Titip Barang</h2>
+            <br>
+            <table>
+                <tr>
+                    <td style="width: 250px;">Nama Barang</td>
+                    <td>:</td>
+                    <td><input type="text" placeholder="Masukkan Nama Barang" style="width: 255px;"></td>
+                </tr>
+                <tr>
+                    <td>Jumlah Barang</td>
+                    <td>:</td>
+                    <td><select id="jumlahBarang" name="jumlahBarang"> 
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </select></td>
+                </tr>
+                <tr>
+                    <td>Deskripsi Barang</td>
+                    <td>:</td>
+                    <td>
+                        <textarea name="" id="" cols="30" rows="5"></textarea>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Dapat ditemukan di</td>
+                    <td>:</td>
+                    <td><input type="text" placeholder="Ditemukan di" style="width: 255px;"></td>
+                </tr>
+            </table>
+            <br>
+            <h4>Upload Gambar</h4>
+            <input type="image" src="" alt="">
+            <button>Browse</button>
+            
+            <br>
+            <br>
+            <br>
+            <br> 
+
+            <div class="w3-right" style="padding-top: 100px;"> 
+                <div class="w3-btn" style="background-color:#b74449; color: white;">Submit</div>
+            </div>
+        </div>
+    </div>
