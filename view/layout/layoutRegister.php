cd61efc1c9731507241c324e7fbccf7e5384e25b
Yulius Famas <yuliusfamas@gmail.com>
2020-04-25 15:23:07 +0700

    add file

--- view/layout/layoutRegister.php
+++ view/layout/layoutRegister.php
@@ -12,7 +12,6 @@
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="style/style.css">
     <title>Document</title>
 </head>
 <body>
