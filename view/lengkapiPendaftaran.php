98f88909911b11a7b9ea9e4722c4c5b7a2d9c5f0
Yulius Famas <yuliusfamas@gmail.com>
2020-04-25 15:21:22 +0700

    ADD file Lengkapi Pendaftaran

---
+++ view/lengkapiPendaftaran.php
@@ -0,0 +1,44 @@
+<div class="container">
+        <div class="w3-card-4 w3-white" style="width:60%; margin: auto; padding: 50px; height: 750px; ">
+            <h2>Pendaftaran Tambahan</h2>
+            <br>
+            <br>
+            <br>
+            <form action="#" method="POST" enctype="multipart/form-data">
+                <div class="container"> 
+                    <label for="nik"><b>NIK</b></label>
+                    <input type="text" placeholder="" name="email">
+                    <br>
+                    <label for="bank"><b>Nama Bank</b></label>
+                    <br>
+                    <br>
+                    <select id="bank" name="namaBank"> 
+                        <option>BCA</option>
+                        <option>BRI</option>
+                        <option>BNI</option>
+                        <option>OCBC</option>
+                        <option>MANDIRI</option>
+                    </select>
+                    <br>
+                    <br>
+                    <label for="noRek"><b>No. Rekening</b></label>
+                    <br>
+                    <input type="text" placeholder="" name="noRek">
+                    <br>
+                    <h4>Upload Foto KTP</h4>
+                    <input type='file' name='foto' accept='image/*'>  
+                    <br>
+                     
+                </div>
+
+                <br>
+                <br>
+                <br> 
+
+                <div class="w3-right" style="padding-top: 100px;"> 
+                    <div class="w3-btn" style="background-color:#b74449; color: white;">Submit</div>
+                </div>
+
+            </form>
+        </div>
+    </div>
