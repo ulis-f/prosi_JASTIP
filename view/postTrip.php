0afda918d638d5f85f95a17d83337ac5c47fb8a1
Yulius Famas <yuliusfamas@gmail.com>
2020-04-25 15:21:55 +0700

    ADD file post trip

---
+++ view/postTrip.php
@@ -0,0 +1,35 @@
+<div class="container">
+        <div class="w3-card-4 w3-white" style="width:60%; margin: auto; padding: 50px; height: 650px; ">
+            <form action="" method="POST" enctype="multipart/form-data">
+                <h2>Perjalanan</h2>
+                <br>
+                <table>
+                    <h4>Rute</h4>
+                    <tr>
+                        <td><input type="text" placeholder="Asal" style="width: 255px;"></td>
+                        <td><i class="fa fa-angle-double-right" style="font-size:24px"></i></td>
+                        <td><input type="text" placeholder="Tujuan" style="width: 255px;"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" placeholder="Waktu Keberangkatan"></td>
+                        <td></td>
+                        <td><input type="text" placeholder="Waktu Tiba"></td>
+                    </tr>
+                </table>
+                <br>
+                <h4>Upload Tiket</h4>
+                <input type='file' name='foto' accept='image/*'>  
+                
+                
+                <br>
+                <br>
+                <br>
+                <br> 
+
+                <div class="w3-right" style="padding-top: 100px;"> 
+                    <div class="w3-btn" style="background-color:#b74449; color: white;">Cancel</div>
+                    <div class="w3-btn" style="background-color:#b74449; color: white;">Submit</div>
+                </div>
+            </form>
+        </div>
+    </div>
