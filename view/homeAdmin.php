aa95b753a6b1be36ce25ea2ddec69d531007b274
Yulius Famas <yuliusfamas@gmail.com>
2020-05-02 16:03:20 +0700

    UPDATE

--- view/homeAdmin.php
+++ view/homeAdmin.php
@@ -1,4 +1,14 @@
-    <div class="w3-container">  
+<div class="menuAdmin">
+    <ul>
+        <li><a class="active" href="#">Post Trip</a></li>
+        <li><a href="#">Live Chat</a></li>
+        <li><a href="#">Form Barang</a></li>
+        <li><a href="#">Pembayaran</a></li>
+        <li><a href="#">Mengirim Uang</a></li>
+    </ul>
+</div>
+
+<div class="w3-container">  
         <div id="postTrip" class="tabs w3-container"> 
             <div class="" style="width: 80%; margin-left: 10%; margin-right: 10%; margin-top: 5%;" >
 				<div class="w3-card-4 w3-white" style="width:30%; float: left;margin-right: 5%; padding: 15px;" onclick="document.getElementById('persetujuan').style.display='block'" >
