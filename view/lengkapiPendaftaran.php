bdbb209aaa2a1b43c3871b0371a32fb1d024ccc1
stefanhermanus <47750198+stefanhermanus@users.noreply.github.com>
2020-05-03 23:02:24 +0700

    Update lengkapiPendaftaran.php

--- view/lengkapiPendaftaran.php
+++ view/lengkapiPendaftaran.php
@@ -8,7 +8,7 @@
             <form action="lengkapKlik" method="POST" enctype="multipart/form-data">
                 <div class="container"> 
                     <label for="nik"><b>NIK</b></label>
-                    <input type="text" placeholder="" name="nik">
+                    <input type="text" placeholder="no KTP" name="nik">
                     <br>
                     <label for="bank"><b>Nama Bank</b></label>
                     <br>
@@ -25,10 +25,10 @@
                     <label for="noRek"><b>No. Rekening</b></label>
                     <br>
                     <div style="font-size: 10px;">(Contoh input: BCA: 123-456-7891, BNI: 12-3456-7891 ,BRI: 1234-56-789123-45-6, OCBC: 123.45.6789.123, MANDIRI: 123-45-6789123-4 )</div>
-                    <input type="text" placeholder="" name="noRek">
+                    <input type="text" placeholder="Nomor Rekening" name="noRek">
                     <br>
                     <h4>Upload Foto KTP</h4> 
-                    <div style="font-size: 10px;">(Maksimum 10MB)</div>
+                    <div style="font-size: 10px;">(Maksimum Size: 10MB)</div>
                     <input type='file' name="fotoKTP" id="fotoKTP" accept='image/*'>  
                     <br>
                     <h4>Upload Foto Swafoto</h4>  
