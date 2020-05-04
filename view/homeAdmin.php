5b44cde0577676a0de5f909cbfbe0f82cb15e766
stefanhermanus <47750198+stefanhermanus@users.noreply.github.com>
2020-05-04 09:18:32 +0700

    Update homeAdmin.php

--- view/homeAdmin.php
+++ view/homeAdmin.php
@@ -30,3 +30,41 @@
 			</div>
         </div>
 </div>
+
+<div class="w3-container">  
+        <div id="postTrip" class="tabs w3-container"> 
+            <div class="" style="width: 80%; margin-left: 10%; margin-right: 10%; margin-top: 5%;" >
+		    <table>
+                    <tr>
+                        <th>Name</th>
+                        <th>Gambar KTP</th>
+                        <th>Swafoto</th>
+                        <th>Nama Bank</th>
+                        <th>No Rek</th>
+                        <th>No KTP</th>
+                        <th>Action</th>
+                    </tr>
+                    <?php
+                        $conn = mysqli_connect("localhost","root","","titipaja");
+                        if ($conn-> connect_error){
+                            die("connection failed:" . $conn-> connect_error);
+                        }
+
+                        $sql = "SELECT idUser, namaUser, gambarKTP, swafoto, namaBank, norek, noKTP from user WHERE isTraveller LIKE 'pending'";
+                        $result = $conn-> query($sql);
+
+                        if($result-> num_rows > 0){
+                            while($row = $result-> fetch_assoc()){
+                                echo "<tr><td>" . $row["namaUser"] ."</td><td>" . $row["gambarKTP"] . "</td><td>" . $row["swafoto"] ."</td><td>" . $row["namaBank"] ."</td><td>" . $row["norek"] ."</td><td>" . $row["noKTP"] ."</td><td> <button type='button' value='".$row["idUser"] ."'>Decline</button><button type='button'>Confirm</button></td></tr>";
+                            }
+                            echo "</table>";
+                        }else{
+                            echo "0 result";
+                        }
+
+                        $conn-> close();
+                    ?>
+                </table>
+	    </div>
+        </div>
+</div>
