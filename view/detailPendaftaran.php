66c2396c6f14dc273bce3859268c043d744a1858
Yulius Famas <yuliusfamas@gmail.com>
2020-05-09 17:26:27 +0700

    ADD FILE

---
+++ view/detailPendaftaran.php
@@ -0,0 +1,28 @@
+    <div class="container">
+        <div class="w3-card-4 w3-white" style="width:60%; margin: auto; padding: 50px; height: 1100px; background-color: #ebebeb; ">
+            <form action="#" method="POST">
+                <h2>Persetujuan Pendaftaran</h2>
+                    <br>
+                    foto ktp disini
+                    swafoto
+                    <br>
+                    <label for=""><b> NIK</b></label>
+                    <br>
+                    noNik
+                    <br>
+                    <label for=""><b>Nama Bank</b></label>
+                    <br>
+                    namaBank
+                    <br>
+                    <label for=""><b>No. Rekening</b></label>
+                    <br>
+                    <input type="radio" id="verified" name="verified" value="verified">Verified<br>
+                    <input type="radio" id="unverified" name="unverified" value="unverified">Unverified
+                    <br> <br>
+                    <label for="note">Note</label> <br>
+                    <textarea name="" id="" cols="30" rows="5"></textarea>
+                    <br><br><br><br><br>
+                    <div class="w3-btn w3-right">Submit</div>
+            </form>    
+        </div>
+    </div>
---
+++ view/persetujuanPendaftaran.php
@@ -0,0 +1,19 @@
+<div class="container">
+    <h2>Lengkapi Pendaftaran</h2>        
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Nama</th>
+                <th>Email</th>
+                <th>Persetujuan</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>John</td>
+                <td>john@example.com</td>
+                <td><button>Detail</button></td>  
+            </tr>
+          </tbody>
+    </table>
+</div>  
