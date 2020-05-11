5c41163906238a99eb5c59cc9b1e286aef8a77e6
Yulius Famas <yuliusfamas@gmail.com>
2020-05-11 20:37:43 +0700

    UPDATE DETAIL PENDAFTARAN

--- view/detailPendaftaran.php
+++ view/detailPendaftaran.php
@@ -1,26 +1,34 @@
+<style>
+    figure:hover{
+    transform: scale(2.4);
+}
+</style>
+  
     <div class="container">
-        <div class="w3-card-4 w3-white" style="width:60%; margin: auto; padding: 50px; height: 1100px; background-color: #ebebeb; ">
+        <div class="w3-card-4 w3-white" style="width:60%; margin: auto; padding: 50px; height: 1200px; margin-top: 15%;">
             <form action="verifikasiPendaftaran" method="POST">
-                <h2>Persetujuan Pendaftaran</h2>
-                    <br>
+                <h2>Persetujuan Pendaftaran</h2> 
+                    <br> 
                     <?php
                     foreach($result as $key=>$value){
                     echo"<input type='hidden' name='id' value='".$value->IDuser."'>";
-                    echo"<img src='image/trip/".$value->gambarktp."'width=560px height=300px>";
-                    echo"<img src='image/trip/".$value->swafoto."'width=560px height=300px>";
+                    echo"<label for=''><b>Foto KTP</b></label> <br>";
+                    echo"<figure><img src='image/trip/".$value->gambarktp."'width=250px height=150px></figure><br>";
+                    echo"<label for=''><b>Swafoto</b></label> <br>";
+                    echo"<figure><img src='image/trip/".$value->swafoto."'width=250px height=150px></figure>";
                     echo "<br>
                     <label for=''><b> NIK</b></label>
                     <br>";
-                    echo "$value->noktp";
-                    echo "<br>
+                    echo "$value->noktp"; 
+                    echo "<br><br>
                     <label for=''><b>Nama Bank</b></label>
                     <br>";
                     echo"$value->namabank";
-                    echo"<br>
+                    echo"<br><br>
                     <label for=''><b>No. Rekening</b></label>
                     <br>";
                     echo"$value->norek";
-                    echo"<br>";
+                    echo"<br><br>";
                     echo"<input type='radio' id='verified' name='verified' value='verified'>Verified<br>
                     <input type='radio' id='unverifie' name='verified' value='unverified'>Unverified
                     <br> <br>
