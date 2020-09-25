8843c6d98d15708a04ece30f2a560671395738b5
Yulius Famas <yuliusfamas@gmail.com>
2020-09-26 02:12:04 +0700

    ADD FILE

---
+++ view/detailPersetujuanBarang.php
@@ -0,0 +1,52 @@
+<style>
+    figure:hover{
+        transform: scale(2.4); 
+    }
+</style>
+  
+    <div class="container">
+        <div class="w3-card-4 w3-white" style="width:60%; margin: auto; padding: 50px; height: 1200px; margin-top: 15%;">
+            <form action="" method="POST">
+                <h2>Persetujuan Barang</h2> 
+                    <table>
+                        <tr>
+                            <td><label for="namaBarang">Nama Barang</label></td>
+                            <td>:</td>
+                            <td>Nama barangnya di sini</td>
+                        </tr>
+                        <tr>
+                            <td><label for="kategoriBarang">Kategori Barang</label></td>
+                            <td>:</td>
+                            <td>nama kategori barang</td>
+                        </tr>
+                        <tr>
+                            <td><label for="hargaBarang">Harga Barang</label></td>
+                            <td>:</td>
+                            <td>harga barang di sini</td>
+                        </tr>
+                        <tr>
+                            <td><label for="namaNegara">Nama Negara</label></td>
+                            <td>:</td>
+                            <td>nama negara di sini</td> 
+                        </tr>
+                        <tr>
+                            <td><label for="deskripsiBarang">Deskripsi Barang</label></td>
+                            <td>:</td>
+                            <td>Deskripsi barang di sini</td>
+                        </tr>
+                    </table>
+                    <br>
+                    <label for="gambarBarang">Gambar Barang</label>
+                    <figure><img src=''width=250px height=150px></figure><br> 
+                    <br>
+                    <input type='radio' id='verified' name='verified' value='verified'>Verified<br>
+                    <input type='radio' id='unverified' name='verified' value='unverified'>Unverified<br>
+                    <br>
+                    <label for='note'>Note</label> <br>
+                    <textarea name='' id='' cols='30' rows='5'></textarea>
+                    <br><br><br><br><br>";
+                    <input type='submit' class='w3-btn w3-right w3-theme'>
+
+            </form>    
+        </div>
+    </div>
