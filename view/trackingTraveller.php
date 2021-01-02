194995a6738c79e5803f901cffa05207e6e80965
Yulius Famas <yuliusfamas@gmail.com>
2021-01-02 23:19:54 +0700

    update tracking traveller

--- view/trackingTraveller.php
+++ view/trackingTraveller.php
@@ -17,6 +17,161 @@
     </li>
 </ul>
 
+<fieldset class="" style="margin-left:2%; margin-right:2%; border: #6699cc 1px solid">
+<div class="container">
+        <div class="w3-card-4 w3-white" style="padding: 50px; height: 800px; margin-top: 2%;">
+
+            <div class="w3-bar" style="margin-bottom: 3%;">
+                <h5><b>Status Pesanan</b></h5>
+                <button class="btn btn-outline-success tablink w3-green" type="button" style="border-bottom-left-radius: 25px; 
+                border-top-left-radius: 25px; border-bottom-right-radius: 25px; border-top-right-radius: 25px;"  onclick="openContent(this,'id1')">Pesanan Diproses</button>
+                <button class="btn btn-outline-success tablink" type="button" style="border-bottom-left-radius: 25px; 
+                border-top-left-radius: 25px; border-bottom-right-radius: 25px; border-top-right-radius: 25px;" onclick="openContent(this,'id2')">Pesanan Dikirim Ke Indonesia</button>
+                <button class="btn btn-outline-success tablink" type="button" style="border-bottom-left-radius: 25px; 
+                border-top-left-radius: 25px; border-bottom-right-radius: 25px; border-top-right-radius: 25px;" onclick="openContent(this,'id3')">Pesanan Tiba Di Indonesia</button>
+                <button class="btn btn-outline-success tablink" type="button" style="border-bottom-left-radius: 25px; 
+                border-top-left-radius: 25px; border-bottom-right-radius: 25px; border-top-right-radius: 25px;" onclick="openContent(this,'id4')">Pesanan Dikirim</button>
+                <button class="btn btn-outline-success tablink" type="button" style="border-bottom-left-radius: 25px; 
+                border-top-left-radius: 25px; border-bottom-right-radius: 25px; border-top-right-radius: 25px;" onclick="openContent(this,'id5')">Pesanan Diterima</button>  
+            </div>
+
+            <div id="id1" class="tabs">
+                <?php
+                foreach ($result as $key => $value) {
+                    echo "<fieldset>
+                    <table class=''>
+                        <tr>
+                            <td style='float: left; padding: 3px;'><h5>" . $value->kotaAwal . "</h5></td>
+                            <td style='float: right; padding-right: 10px ;'><h5>" . $value->kotaTujuan . "</h5></td>
+                            <td rowspan='2' style='width: 15%; border-left:#dddddd 1px solid; padding-left:10px;color:#ffa500;font-size:15px;'><b>" . $value->namaBarang . "</b></td>
+                            <td rowspan='2' style='width: 3%; padding: 3px;'><i class='fa fa-angle-double-right' style='font-size:25px;'></i></td>
+                            <td style='width: 40%; padding: 10px; '><input required type='checkbox' name='pesananKirimKeIndo' value='pesananKirimKeIndo'>
+                            <label for='pesananKirimKeIndo' style='font-weight: normal; font-size:13px;'>Ubah status menjadi Pesanan dikirim ke Indonesia</label></td>
+                            
+                        </tr>
+
+                        <tr>
+                            <td style='float:left; padding: 3px;font-size:13px;'><b>" . $value->waktuAwal . "</b></td>
+                            <td style='float: right; padding-right: 10px;font-size:13px;'><b>" . $value->waktuAkhir . "</b></td>
+                            <td><button class='btn btn-primary btn-sm' style='float: right;padding: 9px 7px;border-radius: 25px;'>Ubah Status</button></td> 
+                        </tr>
+                            
+            
+                    
+                            
+                    </table>
+                </fieldset>";
+                }?>
+            </div>
+
+            <div id="id2" class="tabs" style="display: none;">
+                ini halaman pesanan di kirim ke indonesia
+                <?php
+                foreach ($result as $key => $value) {
+                    echo "<fieldset>
+                    <table class=''>
+                        <tr>
+                            <td style='float: left; padding: 3px;'><h5>" . $value->kotaAwal . "</h5></td>
+                            <td style='float: right; padding-right: 10px ;'><h5>" . $value->kotaTujuan . "</h5></td>
+                            <td rowspan='2' style='width: 15%; border-left:#dddddd 1px solid; padding-left:10px;color:#ffa500;font-size:15px;'><b>" . $value->namaBarang . "</b></td>
+                            <td rowspan='2' style='width: 3%; padding: 3px;'><i class='fa fa-angle-double-right' style='font-size:25px;'></i></td>
+                            <td style='width: 40%; padding: 10px; '><input required type='checkbox' name='pesananTibaDiIndo' value='pesananTibaDiIndo'>
+                            <label for='pesananTibaDiIndo' style='font-weight: normal; font-size:13px;'>Ubah status menjadi Pesanan Tiba di Indonesia</label></td>
+                            
+                        </tr>
+
+                        <tr>
+                            <td style='float:left; padding: 3px;font-size:13px;'><b>" . $value->waktuAwal . "</b></td>
+                            <td style='float: right; padding-right: 10px;font-size:13px;'><b>" . $value->waktuAkhir . "</b></td>
+                            <td><button class='btn btn-primary btn-sm' style='float: right;padding: 9px 7px;border-radius: 25px;'>Ubah Status</button></td> 
+                        </tr>
+                            
+                    </table>
+                </fieldset>";
+                }?>
+            </div>
+        
+            <div id="id3" class="tabs" style="display: none">
+                Ini Halaman pesanan tiba di indonesia
+                <?php
+                foreach ($result as $key => $value) {
+                    echo "<fieldset>
+                    <table class=''>
+                        <tr>
+                            <td style='float: left; padding: 3px;'><h5>" . $value->kotaAwal . "</h5></td>
+                            <td style='float: right; padding-right: 10px ;'><h5>" . $value->kotaTujuan . "</h5></td>
+                            <td rowspan='2' style='width: 15%; border-left:#dddddd 1px solid; padding-left:10px;color:#ffa500;font-size:15px;'><b>" . $value->namaBarang . "</b></td>
+                            <td rowspan='2' style='width: 3%; padding: 3px;'><i class='fa fa-angle-double-right' style='font-size:25px;'></i></td>
+                            <td style='width: 40%; padding: 10px; '><input required type='checkbox' name='pesananDikirim' value='pesananDikirim'>
+                            <label for='pesananDikirim' style='font-weight: normal; font-size:13px;'>Ubah status menjadi Pesanan Dikirim</label></td>
+                            
+                        </tr>
+
+                        <tr>
+                            <td style='float:left; padding: 3px;font-size:13px;'><b>" . $value->waktuAwal . "</b></td>
+                            <td style='float: right; padding-right: 10px;font-size:13px;'><b>" . $value->waktuAkhir . "</b></td>
+                            <td>
+                            <input required class='w3-input w3-border w3-border-theme' style='float:left;' placeholder='Masukkan No.Resi' type='text' id='noResi'>
+                            <button class='btn btn-primary btn-sm' style='float: right;padding: 9px 7px;border-radius: 25px;'>Ubah Status</button>
+                            </td> 
+                        </tr>
+                            
+                    </table>
+                </fieldset>";
+                }?>
+            </div>  
+
+            <div id="id4" class="tabs" style="display: none;">
+                <?php
+                foreach ($result as $key => $value) {
+                    echo "<fieldset>
+                    <table class=''>
+                        <tr>
+                            <td style='float: left; padding: 3px;'><h5>" . $value->kotaAwal . "</h5></td>
+                            <td style='float: right; padding-right: 10px ;'><h5>" . $value->kotaTujuan . "</h5></td>
+                            <td rowspan='2' style='width: 15%; border-left:#dddddd 1px solid; padding-left:10px;color:#ffa500;font-size:15px;'><b>" . $value->namaBarang . "</b></td>
+                            <td rowspan='2' style='width: 3%; padding: 3px;'><i class='fa fa-angle-double-right' style='font-size:25px;'></i></td>
+                            <td style='width: 40%; padding: 10px; '><input required type='checkbox' name='pesananDiterima' value='pesananDiterima'>
+                            <label for='pesananDiterima' style='font-weight: normal; font-size:13px;'>Ubah status menjadi Pesanan Diterima</label></td>
+                            
+                        </tr>
+
+                        <tr>
+                            <td style='float:left; padding: 3px;font-size:13px;'><b>" . $value->waktuAwal . "</b></td>
+                            <td style='float: right; padding-right: 10px;font-size:13px;'><b>" . $value->waktuAkhir . "</b></td>
+                            <td><button class='btn btn-primary btn-sm' style='float: right;padding: 9px 7px;border-radius: 25px;'>Ubah Status</button></td> 
+                        </tr>
+                            
+                    </table>
+                </fieldset>";
+                }?>
+            </div>
+        
+            <div id="id5" class="tabs" style="display: none"> 
+                Ini Halaman diterima
+                <?php
+                foreach ($result as $key => $value) {
+                    echo "<fieldset>
+                    <table class=''>
+                        <tr>
+                            <td style='float: left; padding: 3px;width: 40%;'><h5>" . $value->kotaAwal . "</h5></td>
+                            <td style='float: right; padding-right: 10px ;width: 40%;'><h5>" . $value->kotaTujuan . "</h5></td>
+                            <td rowspan='2' style='width: 40%; border-left:#dddddd 1px solid; padding-left:10px;color:#ffa500;font-size:15px;'><b>" . $value->namaBarang . "</b></td>
+                        </tr>
+  
+                        <tr>
+                            <td style='float:left; padding: 3px;font-size:13px;'><b>" . $value->waktuAwal . "</b></td>
+                            <td style='float: right; padding-right: 10px;font-size:13px;'><b>" . $value->waktuAkhir . "</b></td>
+                        </tr>
+                            
+                    </table>
+                </fieldset>";
+                }?>
+            </div>  
+        </div>
+    </div> 
+</fieldset>
+
 
 <fieldset class="" style="margin-left:2%; margin-right:2%; border: #6699cc 1px solid">
     <div class="container">
@@ -48,10 +203,10 @@
 
                     <tr>";
             
-            foreach ($result as $key => $value) {
+           
                 echo "<td style='float:left; padding: 10px;'><b>" . $value->waktuAwal . "</b></td>
                         <td style='float: right; padding: 10px;'><b>" . $value->waktuAkhir . "</b></td>";
-            }
+            
                 echo"</tr>
                 </table>
             </fieldset>
@@ -75,9 +230,9 @@
 
                                 <form action='statusTracking' method='POST' enctype='multipart/form-data'>";
                                     
-                                    foreach ($result as $key => $value) {
-                                        echo"<input type = 'hidden' name = 'namaBarang' value = '" . $value->namaBarang . "'";
-                                    }
+                                    
+                                    echo"<input type = 'hidden' name = 'namaBarang' value = '" . $value->namaBarang . "'";
+                                    
                             echo"</form>
 
                                 <div class='inner_div'>
